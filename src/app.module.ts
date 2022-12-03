adb1ba3dd04fc52c07bcb207e72a1d60a5042c9a
v.trubicin <v.trubicin@unitcode.ru>
2022-12-03 23:51:35 +0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,33 @@
 import {MiddlewareConsumer, Module, NestModule, RequestMethod} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
-import {TypeOrmModule} from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from "@nestjs/typeorm";
 import {AuthModule} from './auth/auth.module';
 import {UserModule} from './user/user.module';
 import {SharedModule} from './shared/shared.module';
 import {AuthMiddleware} from './auth/middlewares/auth.middleware';
-import {ConfigModule} from '@nestjs/config';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'aurora-data-api'>('TYPEORM_CONNECTION'),
+        host: config.get<string>('TYPEORM_HOST'),
+        port: config.get<number>('TYPEORM_PORT'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        entities: [ __dirname + 'dist/**/*.entity{.ts,.js}' ],
+        synchronize: false,
+        autoLoadEntities: true,
+        logging: true,
+      } as TypeOrmModuleAsyncOptions),
+    }),
     AuthModule,
     UserModule,
     SharedModule,
